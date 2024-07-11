b71058943a58952a8f3bfe6c256f55b7f6e53417
Facundo Kzemin Paez <facupaezk@gmail.com>
2024-07-11 15:26:56 -0300

    Initial Commit

---
+++ Reckless/RecklessApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecklessApp.swift
+//  Reckless
+//
+//  Created by Facundo Kzemin on 11/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct RecklessApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
